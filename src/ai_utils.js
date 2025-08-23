e8d419b0e0f77d3db8b972a43194268113d213f9
range-et <indrajeet.haldar08@gmail.com>
2025-08-23 18:58:14 -0400

    updates to code

--- src/ai_utils.js
+++ src/ai_utils.js
@@ -1,6 +1,21 @@
-async function generateSummary(content) {
-    // TODO: Call to AI service to generate summary
-    return "";
+import OpenAI from "openai";
+import { OPENAI_API_KEY } from "./config";
+
+class OpenAIAgent {
+    constructor() {
+        this.client = new OpenAI({
+            apiKey: OPENAI_API_KEY,
+            dangerouslyAllowBrowser: true
+        });
+    }
+
+    async generateSummary(content) {
+        const response = await this.client.responses.create({
+            model: "gpt-5",
+            input: "Generate a summary of the following content: " + content
+        });
+        return response.output_text;
+    }
 }
 
-export { generateSummary };
+export default OpenAIAgent;
